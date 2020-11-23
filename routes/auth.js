1fd61e5758db9b30a232d0b6aac5977ede530094
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-11-23 16:04:37 -0500

    [auth] Add the sign up post route and tested

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const passport = require('../config/ppConfig');
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -8,5 +10,38 @@ router.get('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+router.post('/signup', (req, res)=> {
+  console.log(req.body);
+  
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  })
+  .then(([user, created])=> {
+    if (created) {
+      //if created/success redirect back to home
+      console.log(`${user.name} was created`);
+      //Flash message
+      const successObject = {
+        successRedirect: '/',
+        successFlash: 'Account created and logging in..'
+      }
+      passport.authenticate('local', successObject) (req, res);
+    } else {
+      //Email already exist
+      req.flash('error', 'Email already exist...')
+      res.redirect('/auth/signup')
+    }
+  })
+  .catch(err => {
+    console.log('Error', err);
+    req.flash('error', 'Either email or password is incorrect. Please try again.');
+    res.redirect('/auth/signup')
+  })
+})
 
 module.exports = router;
